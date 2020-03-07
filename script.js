28680ebc96667208be140e5c091ef66322d64e7f
Temir <example@gmail.com>
2020-03-07 23:00:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+let a = 5;
+console.log(a);
+
+
+
+
+
+
+var one = 1;
+let second = 2;
+const pi = 3.14;
+let lol;
+var obj = {};
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["age"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
+
+
+//alert("Hello bitch");
+
+// let answer = confirm("Are you OK?");
+// console.log(answer);
+
+// let answer = prompt("+18?", "Yes");
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(!!isChecked || isClose);

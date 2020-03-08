87011a4d08cbf3911297ca87c5cc1f9dcc17069a
Temir <example@gmail.com>
2020-03-08 14:51:04 +0300

    if-s and Cycles

--- newscript.js
+++ newscript.js
@@ -4,7 +4,7 @@
 //     console.log("No")
 // }
 
-let num = 50;
+
 
 if (num < 49){
     console.log('no');
@@ -14,5 +14,42 @@ if (num < 49){
     console.log("right");
 }
 
-(num == 50) ? console.log("right") : 
+(num == 50) ? console.log("right") : console.log('no');
 
+switch (num){
+    case num < 49:
+        console.log('no');
+        break;
+    case num > 100:
+        console.log('a lot');
+        break;
+    case num > 80:
+        console.log('still a lot');
+        break;
+    case 50:
+        console.log('yes');
+        break;
+    default:
+        console.log('smthn wrong!');
+        break;
+}
+
+let num = 50;
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+
+for (let i = 1; i < 8; i++){
+    if (i == 6){
+        continue;
+    }
+    console.log(i);
+}

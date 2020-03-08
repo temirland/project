398e4bddc77b36e45f7d20f7d433ea4f95d5b69b
Temir <example@gmail.com>
2020-03-08 14:37:53 +0300

    Cycles

---
+++ newscript.js
@@ -0,0 +1,18 @@
+// if (1){
+//     console.log("Right!")
+// } else{
+//     console.log("No")
+// }
+
+let num = 50;
+
+if (num < 49){
+    console.log('no');
+} else if (num > 100){
+    console.log("a lot");
+} else{
+    console.log("right");
+}
+
+(num == 50) ? console.log("right") : 
+
